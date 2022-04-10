678bae41d266f6ec70951cc9a73d378a067b6cfb
MoiZ <techboyy6@gmail.com>
2022-04-10 21:49:07 +0530

    ride History stored, extra details showed, imgs

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import smtplib
 from geopy.distance import great_circle
 from flask import Flask, render_template, request, redirect, url_for
 import pymongo
-import folium
-from folium import Figure
 from playwright.sync_api import sync_playwright
+from time import asctime
 
 app = Flask(__name__)
 client = pymongo.MongoClient(
@@ -16,11 +15,13 @@ user_id = ''
 geolocator = Nominatim(user_agent="Carpool")
 user_Query = False
 flag = 0
+travel_history = {}
 reg_no = ''
 user_type = 'Traveller'
 
 
 def checkUser(username, userEmail):
+    """Checks whether the user data already exists in db"""
     if(data.find_one({'_id': username, 'email': userEmail})):
         return True
     else:
@@ -28,6 +29,7 @@ def checkUser(username, userEmail):
 
 
 def checkLoc(x, y):
+    """Checks whether the location exists or not"""
     userloc = geolocator.geocode(x)
     ownerloc = geolocator.geocode(y)
     if(bool(userloc) and bool(ownerloc)):
@@ -36,12 +38,16 @@ def checkLoc(x, y):
         return False
 
 
-def genMap(userFrom, ownerFrom):
-
+def genMap(userData, ownerData):
+    """Generates the url for the map"""
     values = {}
+    userFrom = userData['from_loc']
+    ownerFrom = ownerData['from_loc']
     values['distanceBtw'] = checkLoc(userFrom, ownerFrom)
     values['timeTaken'] = values['distanceBtw']/40
     values['cost'] = values['distanceBtw']*100
+    values['Insuarance_Exp'] = ownerData['insaurance_exp']
+    values['Vehicle_Reg'] = ownerData['reg_no']
 
     user = geolocator.geocode(userFrom)
     owner = geolocator.geocode(ownerFrom)
@@ -51,16 +57,6 @@ def genMap(userFrom, ownerFrom):
     oLat = owner.latitude
     oLong = owner.longitude
 
-    # fig = Figure(width=550, height=350)
-    # map = folium.Map(location=[uLat, uLong], zoom_start=10)
-    # fig.add_child(map)
-
-    # folium.Marker(location=[uLat, uLong], popup=userFrom,
-    #               tooltip='Your location').add_to(map)
-    # folium.Marker(location=[oLat, oLong], popup=ownerFrom,
-    #               tooltip='Fellow traveler location').add_to(map)
-
-    # map.save("templates/mainMap.html")
     values['map_url'] = f"https://www.openstreetmap.org/directions?engine=fossgis_osrm_car&route={uLat}%2C{uLong}%3B{oLat}%2C{oLong}&layers=O"
 
     return values
@@ -68,19 +64,20 @@ def genMap(userFrom, ownerFrom):
 
 @app.route('/details')
 def details():
+    global travel_history
     sendMail()
-    connectData = data.find_one({'_id': request.args.get('connectId')})
+    connectID = request.args.get('connectId')
+    connectData = data.find_one({'_id': connectID})
     userData = data.find_one({'_id': user_id})
-    # ownerData = data.find_one({'_id': connectId})
-
-    imp_val = genMap(userData['from_loc'], connectData['from_loc'])
+    travel_history = userData['history']
+    travel_history[asctime()] = connectID
+    data.find_one_and_update({'_id': user_id},
+                             {"$set": {
+                                 'history': travel_history
+                             }})
+    imp_val = genMap(userData, connectData)
 
     return render_template('details.html', ownerData=connectData, travelVal=imp_val)
-
-
-@app.route('/big-map')
-def bigMap():
-    return render_template('mainMap.html')
 
 
 @app.route('/send_mail')
@@ -92,18 +89,18 @@ def sendMail():
     connectData = connectData.next()
     server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
     server.login("testmailcarpool@gmail.com", "carpoolTestMail")
-    connectMsg = '''Subject: Details Requested Alert | Carpool\n\n
+    connectMsg = f"""Subject: Details Requested Alert | Carpool\n\n
 Greetings from Carpool,\n
 The below mentioned person has viewed your details.
-Name : {}
-Phone : {}
-Email : {}
-Gender : {}
-Age : {}
-Destination : {}
+Name : {userData['name']}
+Phone : {userData['mobile']}
+Email : {userData['email']}
+Gender : {userData['gender']}
+Age : {userData['age']}
+Destination : {userData['to_loc']}
 \nKindly please contact them for further clarification
 Thanks & Regards
-Team Carpool'''.format(userData['name'], userData['mobile'], userData['email'], userData['gender'], userData['age'], userData['to_loc'])
+Team Carpool"""
     server.sendmail("testmailcarpool@gmail.com",
                     connectData['email'], connectMsg)
     server.quit()
@@ -131,7 +128,7 @@ def login():
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    global user_id
+    # global user_id
     if(request.method == 'POST'):
         if(user_id != ''):
             srchLoc = geolocator.geocode(request.form.get('search'))
@@ -148,97 +145,77 @@ def home():
 @app.route('/register/', methods=['GET', 'POST'])
 def register():
     if(request.method == 'POST'):
-
-        global user_id
-        global reg_no
-        
         user_password = request.form.get('password')
         user_email = request.form.get('email')
-
-        global flag
-        typeTr = 'Traveller'
-        typeow = 'Owner'
 
         if (not checkUser(user_id, user_email)):
             if(checkLoc(request.form.get('from'), request.form.get('to'))):
                 if(reg_no == ''):
                     data.update_one({'_id': user_id},
-                    {"$set":{
-                        'type': typeTr,
-                        'name': request.form.get('name')
-                    }})
+                                    {"$set": {
+                                        'type': user_type,
+                                        'name': request.form.get('name')
+                                    }})
                 elif(reg_no):
                     data.update_one({'_id': user_id},
-                        {"$set":{
-                            'type': typeow,
-                        }})
+                                    {"$set": {
+                                        'type': 'Owner',
+                                    }})
                 data.update_one({'_id': user_id},
-                    {"$set":{
-                    # 'name': request.form.get('name'),
-                    # 'type': request.form.get('user_type'),
-                    'gender': request.form.get('gender'),
-                    'age': int(request.form.get('age')),
-                    'email': user_email,
-                    'mobile': int(request.form.get('phone')),
-                    'from_loc': request.form.get('from').lower(),
-                    'to_loc': request.form.get('to').lower(),
-                    'password': user_password,
-                }})
+                                {"$set": {
+                                    'gender': request.form.get('gender'),
+                                    'age': int(request.form.get('age')),
+                                    'email': user_email,
+                                    'mobile': int(request.form.get('phone')),
+                                    'from_loc': request.form.get('from').lower(),
+                                    'to_loc': request.form.get('to').lower(),
+                                    'password': user_password,
+                                }})
 
                 return redirect(url_for("home"))
             return render_template("owner.html", location_error="Try other Location")
         return render_template("login.html", register_error="User Already Exist")
-    return render_template('owner.html', type = user_type )
+    return render_template('owner.html', type=user_type)
+
 
 @app.route('/id/', methods=['GET', 'POST'])
 def id():
+    global user_id
     if(request.method == 'POST'):
-        global user_id
         user_id = request.form.get('username')
-        if(not data.find_one({"_id": user_id})):
+        print(user_id)
+        if(not data.find_one({'_id': user_id})):
             data.insert_one({
                 '_id': user_id,
             })
-            return render_template('type.html', user_data = user_id)
-    return render_template('id.html', user_data = user_id)
-
-@app.route('/types', methods=['GET', 'POST'])
-def types():
-    global user_id
-    if(user_id):
-        return render_template("type.html")
-    
-    return render_template("type.html")
-    
+            return render_template('type.html', user_data=user_id)
+    return render_template('id.html', user_data=user_id)
 
 
 @app.route('/modify/', methods=['GET', 'POST'])
 def modify():
     global user_id
+    user_data = data.find_one({'_id': user_id})
     if(request.method == 'POST'):
-        user_data = data.find_one({'_id': user_id})
-
         if(user_data):
             if(checkLoc(request.form.get('from'), request.form.get('to'))):
                 data.update_one({'_id': user_id},
-                                {"$set":{
-                                'type': request.form.get('user_type'),
-                                'age': int(request.form.get('age')),
-                                'email': request.form.get('email'),
-                                'mobile': int(request.form.get('phone')),
-                                'from_loc': request.form.get('from').lower(),
-                                'to_loc': request.form.get('to').lower(),
-                                'password': request.form.get('password'),                                    
+                                {"$set": {
+                                    'type': request.form.get('user_type'),
+                                    'age': int(request.form.get('age')),
+                                    'email': request.form.get('email'),
+                                    'mobile': int(request.form.get('phone')),
+                                    'from_loc': request.form.get('from').lower(),
+                                    'to_loc': request.form.get('to').lower(),
+                                    'password': request.form.get('password'),
                                 }})
                 return redirect(url_for("home"))
-            return render_template("modify.html", location_error = "Try another location")
-    return render_template('modify.html')
+            return render_template("modify.html", location_error="Try another location")
+    return render_template('modify.html', data=user_data)
 
 
 @app.route('/fetch', methods=['GET', 'POST'])
 def fetch():
-
-    global user_id
     global flag
     global reg_no
     global user_type
@@ -256,7 +233,7 @@ def fetch():
         details['name'] = page.query_selector(
             '//html/body/div/div/div[2]/div[2]/p').inner_text()
         details['fuel_type'] = page.query_selector(
-                '//html/body/div/div/div[7]/div[2]/p').inner_text()
+            '//html/body/div/div/div[7]/div[2]/p').inner_text()
         details['insaurance_exp'] = page.query_selector(
             '//html/body/div/div/div[12]/div[2]/p').inner_text()
         reg_no = details['reg_no']
@@ -266,14 +243,15 @@ def fetch():
             user_type = 'Owner'
             data.update_one({
                 '_id': user_id},
-                {"$set":{'reg_no': details['reg_no'],
-                'name': details['name'],
-                'fuel_type': details['fuel_type'],
-                'insaurance_exp': details['insaurance_exp'],
-            }})
+                {"$set": {'reg_no': details['reg_no'],
+                          'name': details['name'],
+                          'fuel_type': details['fuel_type'],
+                          'insaurance_exp': details['insaurance_exp'],
+                          }})
             return redirect(url_for("register"))
 
     return details
+
 
 if __name__ == "__main__":
     app.run(debug=True)
